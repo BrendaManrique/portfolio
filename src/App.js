df08f9e90ae873cc19fc835eca648b76988ec989
BrendaManrique <brenda@users.github.com>
2022-11-22 04:50:06 -0500

    Changes for React GA

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
       resumeData: {}
     };
 
-    ReactGA.initialize("UA-110570651-1");
+    ReactGA.initialize("G-K6DB1Z1NY4");
     ReactGA.pageview(window.location.pathname);
   }
 
